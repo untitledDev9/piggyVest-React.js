2b5ff9583ccd7fa1ea78747ac9bd0581efe50862
untitled <ahmedsuleiman940@gmail.com>
2025-06-20 20:41:34 +0100

    done with savings

--- src/components/savings/Savings.jsx
+++ src/components/savings/Savings.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import SavingsCard from './SavingsCard'
 import './Savings.css'
 import One from "../../assets/images/one.png"
+import Two from "../../assets/images/Two.png"
+import Three from "../../assets/images/three.png"
+import Four from "../../assets/images/four.png"
+import Five from "../../assets/images/five.png"
+import Six from "../../assets/images/six.png"
 
 const Savings = () => {
     return (
@@ -26,10 +31,10 @@ const Savings = () => {
                 />
 
                 <SavingsCard
-                    title="Automated Savings"
-                    description='Build a dedicated savings faster on your terms, automatically or manually.'
-                    label='Piggybank'
-                    image={One}
+                    title="Fixed Savings"
+                    description="Lock money away for a fixed duration without having access to it until maturity. It's like having a custom fixed deposit."
+                    label='Safelock'
+                    image={Two}
                     customClass="col-left"
                 />
             </div>
@@ -38,18 +43,18 @@ const Savings = () => {
 
             <div className="savings-col col-2">
                 <SavingsCard
-                    title="Automated Savings"
-                    description='Build a dedicated savings faster on your terms, automatically or manually.'
-                    label='Piggybank'
-                    image={One}
+                    title="Goal-oriented Saving"
+                    description="Reach all your savings goals faster. Save towards multiple goals on your own or with a group."
+                    label='Target Savings'
+                    image={Three}
                     customClass="col-right-2"
                 />
 
                 <SavingsCard
-                    title="Automated Savings"
-                    description='Build a dedicated savings faster on your terms, automatically or manually.'
-                    label='Piggybank'
-                    image={One}
+                    title="Flex Naira"
+                    description='Save, transfer, manage, organise, and withdraw your money at any time.'
+                    label='Flex Naira'
+                    image={Four}
                     customClass="col-left-2"
                 />
             </div>
@@ -58,18 +63,19 @@ const Savings = () => {
 
             <div className="savings-col col-3">
                 <SavingsCard
-                    title="Automated Savings"
-                    description='Build a dedicated savings faster on your terms, automatically or manually.'
-                    label='Piggybank'
-                    image={One}
+                    title="Flex Dollar"
+                    description='Save and grow your money in foreign currencies such as Dollars.
+'
+                    label='Flex Dollar'
+                    image={Five}
                     customClass="col-right-3"
                 />
 
                 <SavingsCard
-                    title="Automated Savings"
-                    description='Build a dedicated savings faster on your terms, automatically or manually.'
-                    label='Piggybank'
-                    image={One}
+                    title="HouseMoney"
+                    description='Plan for your rent and household expenses'
+                    label='House Money'
+                    image={Six}
                     customClass="col-left-3"
                 />
             </div>
