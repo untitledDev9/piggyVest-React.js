a66c2678428d8b116b64b2db74a30888f5466c04
untitled <ahmedsuleiman940@gmail.com>
2025-06-20 19:10:29 +0100

    done with join  section

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import NavBar from "../components/navbar/NavBar"
 import Hero from "../components/hero/Hero"
+import Invest from '../components/invest/Invest'
+import Month from "../components/month/Month"
+import Join from '../components/join/Join'
 
 const Homepage = () => {
     return (
         <div>
             <NavBar />
             <Hero />
+            <Invest />
+            <Month />
+            <Join />
         </div>
     )
 }
